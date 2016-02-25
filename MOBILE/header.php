e1c36505c559115fb06d13ad2d5e6e0efcc82a4c
kyh <kyhfan@gmail.com>
2016-02-25 22:04:35 +0900

    1111

--- MOBILE/header.php
+++ MOBILE/header.php
@@ -1,5 +1,8 @@
 <?
 	include_once "../config.php";
+
+	if (!isset($_REQUEST['serial']))
+		$serial2	= $_REQUEST['serial'];
 ?>
 <!doctype html>
   <html>
@@ -7,6 +10,11 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, user-scalable=yes,initial-scale=1.0, maximum-scale=1.0"/>
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta property="og:title" content="[베비언스] 베비언스 먹고 폭풍 성장">
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="http://grow.babience-event.com/MOBILE/index.php?serial=<?=$serial2?>" />
+      <meta property="og:image" content="http://grow.babience-event.com/files/<?=$serial2?>/medium/final_<?=$serial2?>_1.jpg" />
+      <meta property="og:description" content="우리 아기의 성장 과정을 특별하게 담는 방법! 지금 베비언스에서 만들어 드립니다.">
       <link rel="shortcut icon" type="image/x-icon" href="./images/fabicon.ico" />
       <title>베비언스 먹고 폭풍 성장!</title>
       <link href="css/style.css" rel="stylesheet" type="text/css">
