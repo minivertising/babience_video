7b8d659c523057658ec0243ba91945137e446dbe
kyh <kyhfan@gmail.com>
2016-02-25 22:01:13 +0900

    1111

--- MOBILE/header.php
+++ MOBILE/header.php
@@ -1,6 +1,5 @@
 <?
 	include_once "../config.php";
-	$serial	= $_REQUEST['serial'];
 ?>
 <!doctype html>
   <html>
@@ -8,11 +7,6 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, user-scalable=yes,initial-scale=1.0, maximum-scale=1.0"/>
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta property="og:title" content="[베비언스] 베비언스 먹고 폭풍 성장">
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="http://grow.babience-event.com/MOBILE/index.php" />
-      <meta property="og:image" content="http://grow.babience-event.com/files/<?=$serial?>/medium/final_<?=$serial?>_1.jpg" />
-      <meta property="og:description" content="우리 아기의 성장 과정을 특별하게 담는 방법! 지금 베비언스에서 만들어 드립니다.">
       <link rel="shortcut icon" type="image/x-icon" href="./images/fabicon.ico" />
       <title>베비언스 먹고 폭풍 성장!</title>
       <link href="css/style.css" rel="stylesheet" type="text/css">
--- MOBILE/share_page.php
+++ MOBILE/share_page.php
@@ -1,11 +1,17 @@
 <?
 	include_once "../config.php";
+	$serial	= $_REQUEST['serial'];
 ?>
 <!doctype html>
   <html>
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, user-scalable=yes,initial-scale=1.0, maximum-scale=1.0"/>
+      <meta property="og:title" content="[베비언스] 베비언스 먹고 폭풍 성장">
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="http://grow.babience-event.com/MOBILE/index.php" />
+      <meta property="og:image" content="http://grow.babience-event.com/files/<?=$serial?>/medium/final_<?=$serial?>_1.jpg" />
+      <meta property="og:description" content="우리 아기의 성장 과정을 특별하게 담는 방법! 지금 베비언스에서 만들어 드립니다.">
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <link rel="shortcut icon" type="image/x-icon" href="./images/fabicon.ico" />
       <title>베비언스 먹고 폭풍 성장!</title>
@@ -13,3 +19,4 @@
       <script type="text/javascript" src="../js/jquery-1.11.2.min.js"></script>
       <script type="text/javascript" src="../js/jquery.easing.1.3.js"></script>
     </head>
+	<body>	</body></html>
