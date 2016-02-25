2139b92f2d4bd6eabc183c512702a551f5da828c
kyh <kyhfan@gmail.com>
2016-02-25 15:50:49 +0900

    DB 암호 변경

--- include/dbi.php
+++ include/dbi.php
@@ -8,7 +8,7 @@
      * Created : 2014
      *
      ******************************************************************************/
-	$my_db = new mysqli("localhost", "root", "6alslqjxkdlwld@%*)", "babience_video");
+	$my_db = new mysqli("localhost", "root", "7alslqjxkdlwld@%*)", "babience_video");
 	if (mysqli_connect_error()) {
 		exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
 	}
