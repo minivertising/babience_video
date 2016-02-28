627171c07c491f6653383f30e290923d973fc0ac
kyh <kyhfan@gmail.com>
2016-02-28 14:05:14 +0900

    111

--- MOBILE/shell_test.php
+++ MOBILE/shell_test.php
@@ -1,4 +1,16 @@
 <?
+/*
+$mydir = "4C12029A69"; 
+ if(@mkdir($mydir, 0777)) { 
+    if(is_dir($mydir)) { 
+        @chmod($mydir, 0777); 
+        echo "${mydir} 디렉토리를 생성하였습니다."; 
+    } 
+ } 
+ else { 
+    echo "${mydir} 디렉토리를 생성하지 못했습니다."; 
+ } 
+*/
 
 $output	= "ffmpeg \\";
 $output	.= "-loop 1 -t 1 -i ../files/4C12029A69/medium/final_4C12029A69_1.jpg \\";
@@ -12,7 +24,7 @@ $output	.= "'A*(if(gte(T,0.5),1,T/0.5))+B*(1-(if(gte(T,0.5),1,T/0.5)))'[b1v]; \\
 $output	.= " [2:v][1:v]blend=all_expr='A*(if(gte(T,0.5),1,T/0.5))+B*(1-(if(gte(T,0.5),1,T/0.5)))'[b2v]; \\";
 $output	.= " [3:v][2:v]blend=all_expr='A*(if(gte(T,0.5),1,T/0.5))+B*(1-(if(gte(T,0.5),1,T/0.5)))'[b3v]; \\";
 $output	.= " [4:v][3:v]blend=all_expr='A*(if(gte(T,0.5),1,T/0.5))+B*(1-(if(gte(T,0.5),1,T/0.5)))'[b4v]; \\";
-$output	.= ' [0:v][b1v][1:v][b2v][2:v][b3v][3:v][b4v][4:v]concat=n=9:v=1:a=0,format=yuv420p[v]" -pix_fmt yuv420p -map "[v]"  4C12029A69/out.mp4';
+$output	.= ' [0:v][b1v][1:v][b2v][2:v][b3v][3:v][b4v][4:v]concat=n=9:v=1:a=0,format=yuv420p[v]" -pix_fmt yuv420p -map "[v]"  ../files/4C12029A69/out.mp4';
 
 	
 	system(stripslashes($output));
