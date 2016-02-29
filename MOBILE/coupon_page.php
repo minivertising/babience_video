ada6d5f1dc8ac4c60d7a1d4b6d263361e78c7cd9
kyh <kyhfan@gmail.com>
2016-02-29 12:57:18 +0900

    1111

--- MOBILE/coupon_page.php
+++ MOBILE/coupon_page.php
@@ -62,4 +62,78 @@
 $(document).ready(function(){
 	$("#video_player").width($(".youtube").width());
 });
+
+function sns_share(media, flag)
+{
+	if (media == "fb")
+	{
+
+		var newWindow = window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent('http://grow.babience-event.com/MOBILE/share_page.php?serial=<?=$serial?>'),'sharer','toolbar=0,status=0,width=600,height=325');
+		$.ajax({
+			type   : "POST",
+			async  : false,
+			url    : "../main_exec.belif",
+			data:{
+				"exec" : "insert_share_info",
+				"sns_media" : media,
+				"sns_flag"		: flag
+			}
+		});
+		//var newWindow = window.open('https://www.facebook.com/dialog/feed?app_id=1604312303162602&display=popup&caption=testurl&link=http://vacance.babience-event.com&redirect_uri=http://www.hanatour.com','sharer','toolbar=0,status=0,width=600,height=325');
+	}else if (media == "tw"){
+		var newWindow = window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent("빌리의 수분 폭탄 공장에 숨어 있는 빌리를 찾아주신 분에게는 즉석 당첨을 통해 수분 폭탄 쿠션 체험 키트를 드립니다. ") + '&url='+ encodeURIComponent('http://bit.ly/1QuvGJU'),'sharer','toolbar=0,status=0,width=600,height=325');
+		$.ajax({
+			type   : "POST",
+			async  : false,
+			url    : "../main_exec.belif",
+			data:{
+				"exec" : "insert_share_info",
+				"sns_media" : media,
+				"sns_flag"		: flag
+			}
+		});
+	}else if (media == "kt"){
+		// 카카오톡 링크 버튼을 생성합니다. 처음 한번만 호출하면 됩니다.
+		//Kakao.Link.createTalkLinkButton({
+		Kakao.Link.sendTalkLink({
+		  //container: '#kakao-link-btn',
+		  label: "우리아기 특별한 성장 영상 공개!",
+		  image: {
+			src: 'http://grow.babience-event.com/<?=$serial?>/medium/final_<?=$serial?>_1.jpg',
+			width: '800',
+			height: '600'
+		  },
+		  webButton: {
+			text: '[베비언스] 베비언스 먹고 폭풍 성장!',
+			url: 'http://grow.babience-event.com/MOBILE/index.php?serial=<?=$serial?>' // 앱 설정의 웹 플랫폼에 등록한 도메인의 URL이어야 합니다.
+		  }
+		});
+		$.ajax({
+			type   : "POST",
+			async  : false,
+			url    : "../main_exec.belif",
+			data:{
+				"exec" : "insert_share_info",
+				"sns_media" : media,
+				"sns_flag"		: flag
+			}
+		});
+	}else{
+		Kakao.Story.share({
+			url: 'http://grow.babience-event.com/MOBILE/share_page.php?serial=<?=$serial?>',
+			text: '우리아기 특별한 성장 영상 공개!\r\nhttp://grow.babience-event.com/mypage.php?serial=<?=$serial?>'
+		});
+		$.ajax({
+			type   : "POST",
+			async  : false,
+			url    : "../main_exec.belif",
+			data:{
+				"exec" : "insert_share_info",
+				"sns_media" : media,
+				"sns_flag"		: flag
+			}
+		});
+	}
+}
+
 </script>
